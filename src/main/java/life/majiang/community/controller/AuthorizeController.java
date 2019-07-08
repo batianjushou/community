b84d101d78f21f9cfc968822fecc84fe0a1482fd
batianjushou <735955183@qq.com>
2019-07-08 22:40:56 +0800

    实现持久登录的功能

--- src/main/java/life/majiang/community/controller/AuthorizeController.java
+++ src/main/java/life/majiang/community/controller/AuthorizeController.java
@@ -11,7 +11,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.util.UUID;
 
@@ -30,7 +32,9 @@ public class AuthorizeController {
     @Value("${github.redirect.uri}")
     private String redirectUri;
     @RequestMapping("/callback")
-    public String callBack(@RequestParam(name="code")String code, @RequestParam(name="state")String state, HttpServletRequest request){
+    public String callBack(@RequestParam(name="code")String code,
+                           @RequestParam(name="state")String state,
+                           HttpServletRequest request,HttpServletResponse response){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.setClient_id(clientId);
@@ -42,8 +46,8 @@ public class AuthorizeController {
         //System.out.println(gitHubUser.getName()+":"+user.getBio()+":"+user.getId());
         if(githubUser!=null){
             System.out.println("登录成功，写session  coocie");
-            HttpSession session = request.getSession();
-            session.setAttribute("user",githubUser);
+        /*    HttpSession session = request.getSession();
+            session.setAttribute("user",githubUser);*/
             //写入数据库
             User user = new User();
             user.setToken(UUID.randomUUID().toString());
@@ -54,6 +58,7 @@ public class AuthorizeController {
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insertUser(user);
+            response.addCookie(new Cookie("token",token));
             return "redirect:/";
         }else{
             System.out.println("登录失败");
--- src/main/java/life/majiang/community/mapper/UserMapper.java
+++ src/main/java/life/majiang/community/mapper/UserMapper.java
@@ -3,10 +3,15 @@ package life.majiang.community.mapper;
 import life.majiang.community.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
         @Insert("insert  into user (name,account_Id,token,gmt_Create,gmt_Modified) values(#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified})")
         public void insertUser(User user);
+        //通过token拿到user
+        @Select("select * from user where token=#{token}")
+        User findUserByToken(@Param("token") String token);
 }
